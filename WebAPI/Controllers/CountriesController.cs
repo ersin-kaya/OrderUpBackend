17b81af19325594f0295e663d4e945e95deac2f8
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-03-12 06:41:09 +0300

    CountriesController icerisine GetById ve Add isimli metodlar eklenmistir.

--- WebAPI/Controllers/CountriesController.cs
+++ WebAPI/Controllers/CountriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Business.Abstract;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -24,6 +25,30 @@ namespace WebAPI.Controllers
         public IActionResult GetAll()
         {
             var result = _countryService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _countryService.GetById(id);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Country country)
+        {
+            var result = _countryService.Add(country);
 
             if (result.Success)
             {
